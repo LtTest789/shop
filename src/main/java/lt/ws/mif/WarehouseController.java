9d5aaca01e1eb1bea860bb1e3d82077a4c88bfeb
Romas Augustinavičius <Romas Augustinaviius>
2017-05-10 00:02:07 +0300

    init

--- src/main/java/lt/ws/mif/WarehouseController.java
+++ src/main/java/lt/ws/mif/WarehouseController.java
@@ -1,6 +1,7 @@
 package lt.ws.mif;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -14,20 +15,21 @@ import java.util.List;
  * Created by Romas on 5/6/2017.
  */
 @RestController
-@RequestMapping(path = "/warehouses/")
+@RequestMapping(path = "/rest/warehouses/")
 public class WarehouseController {
 
     @Autowired
     private RestOperations restTemplate;
-    final String fooResourceUrl = "http://localhost:9001/warehouse/items";
 
+    @Value(value = "${url.ip}")
+    private String url;
 
     @Autowired
     private WarehouseService warehouseService;
 
     @RequestMapping(path = "/addresses", method = RequestMethod.GET)
     public List<WarehouseAdresses> getWarehousesAddresses() {
-        return   warehouseService.getAllwarehouseAddress();
+        return warehouseService.getAllwarehouseAddress();
     }
 
     @RequestMapping(path = "/search", method = RequestMethod.GET)
@@ -36,8 +38,8 @@ public class WarehouseController {
     }
 
     @RequestMapping(path = "/items/to-shop", method = RequestMethod.POST)
-    public  ResponseEntity<ItemState> fromWarehouseToShop(@RequestParam("itemId") Long itemId, HttpServletResponse response) {
-        if(warehouseService.getSpecificItemFromWarehouseToShop(itemId)) {
+    public ResponseEntity<ItemState> fromWarehouseToShop(@RequestParam("itemId") Long itemId, HttpServletResponse response) {
+        if (warehouseService.getSpecificItemFromWarehouseToShop(itemId)) {
             response.setHeader("Item", "/shop/items");
             return new ResponseEntity(ItemState.ITEM_ADDED_TO_DATABSE, HttpStatus.CREATED);
         }
@@ -45,8 +47,8 @@ public class WarehouseController {
     }
 
     @RequestMapping(path = "/items/to-warehouse", method = RequestMethod.POST)
-    public  ResponseEntity<ItemState> fromShopToearehouse(@RequestBody ItemInWarehouses item, HttpServletResponse response) {
-        if(warehouseService.sendItemBackTospecifixWarehouse(item)) {
+    public ResponseEntity<ItemState> fromShopToearehouse(@RequestBody ItemInWarehouses item, HttpServletResponse response) {
+        if (warehouseService.sendItemBackTospecifixWarehouse(item)) {
             response.setHeader("Item", "/warehouse/items");
             return new ResponseEntity(ItemState.RETURNED_TO_WAREHOUSE, HttpStatus.CREATED);
         }
@@ -55,13 +57,14 @@ public class WarehouseController {
 
     @RequestMapping(path = "/item", method = RequestMethod.GET)
     public List<WarehouseForm> getWarehouses() {
-        List<WarehouseForm> returned = restTemplate.getForObject(fooResourceUrl, List.class);
+        String uri = url + "/warehouse/items";
+        List<WarehouseForm> returned = restTemplate.getForObject(uri, List.class);
         return returned;
     }
 
     @RequestMapping(path = "/item/{id}", method = RequestMethod.GET)
     public ResponseEntity<WarehouseForm> getWarehousesItem(@PathVariable("id") Long itemId) {
-        String getItemUrl = "http://localhost:9001/warehouse/items/" + String.valueOf(itemId);
+        String getItemUrl = url + "/warehouse/items/" + String.valueOf(itemId);
         try {
             WarehouseForm form = restTemplate.getForObject(getItemUrl, WarehouseForm.class);
             return new ResponseEntity<>(form, HttpStatus.FOUND);
@@ -73,6 +76,7 @@ public class WarehouseController {
     @RequestMapping(path = "/item", method = RequestMethod.POST)
     public ResponseEntity<ItemStateInWarehouse> postToWarehouse(@RequestBody @Valid WarehouseForm warehouseForm, HttpServletResponse response) {
         HttpEntity<WarehouseForm> request = new HttpEntity<>(warehouseForm);
+        String fooResourceUrl = url + "/warehouse/items";
         ItemStateInWarehouse entity = restTemplate.postForObject(fooResourceUrl, request, ItemStateInWarehouse.class);
         if (entity.equals(ItemStateInWarehouse.ITEM_ADDED_TO_WAREHOUSE)) {
             response.setHeader("warehouse-item", "/warehouses/item");
@@ -83,10 +87,10 @@ public class WarehouseController {
 
     @RequestMapping(path = "/item/{id}", method = RequestMethod.PUT)
     public ResponseEntity<ItemStateInWarehouse> putWarehousesItem(@PathVariable("id") Long itemId, @RequestBody @Valid WarehouseForm warehouseForm, HttpServletResponse response) {
-        String targetUrl = "http://localhost:9001/warehouse/items/" + String.valueOf(itemId);
+        String targetUrl =  url + "/warehouse/items/" + String.valueOf(itemId);
         HttpEntity<WarehouseForm> requestEntity = new HttpEntity<>(warehouseForm, new HttpHeaders());
-        ResponseEntity<ItemStateInWarehouse> form = restTemplate.exchange(targetUrl, HttpMethod.PUT, requestEntity ,ItemStateInWarehouse.class);
-        if(ItemStateInWarehouse.ITEM_DELETED_FROM_DATABASE.equals(form)) {
+        ResponseEntity<ItemStateInWarehouse> form = restTemplate.exchange(targetUrl, HttpMethod.PUT, requestEntity, ItemStateInWarehouse.class);
+        if (ItemStateInWarehouse.ITEM_DELETED_FROM_DATABASE.equals(form)) {
             return new ResponseEntity<>(form.getBody(), HttpStatus.ACCEPTED);
         } else {
             return new ResponseEntity<>(form.getBody(), HttpStatus.NOT_FOUND);
@@ -95,10 +99,10 @@ public class WarehouseController {
 
     @RequestMapping(path = "/item/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<ItemStateInWarehouse> deleteWarehousesItem(@PathVariable("id") Long itemId) {
-        String targetUrl = "http://localhost:9001/warehouse/items/" + String.valueOf(itemId);
+        String targetUrl =  url + "/warehouse/items/" + String.valueOf(itemId);
         HttpEntity<?> requestEntity = new HttpEntity<Object>(new HttpHeaders());
-        ResponseEntity<ItemStateInWarehouse> form = restTemplate.exchange(targetUrl, HttpMethod.DELETE, requestEntity ,ItemStateInWarehouse.class);
-        if(ItemStateInWarehouse.ITEM_DELETED_FROM_DATABASE.equals(form)) {
+        ResponseEntity<ItemStateInWarehouse> form = restTemplate.exchange(targetUrl, HttpMethod.DELETE, requestEntity, ItemStateInWarehouse.class);
+        if (ItemStateInWarehouse.ITEM_DELETED_FROM_DATABASE.equals(form)) {
             return new ResponseEntity<>(form.getBody(), HttpStatus.ACCEPTED);
         } else {
             return new ResponseEntity<>(form.getBody(), HttpStatus.NOT_FOUND);
