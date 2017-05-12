266add21b377910df6a51dcff48fb8dfc8ebfa79
Romas Augustinavičius <Romas Augustinaviius>
2017-05-12 09:40:04 +0300

    init

--- src/main/java/lt/ws/mif/WarehouseServiceImpl.java
+++ src/main/java/lt/ws/mif/WarehouseServiceImpl.java
@@ -27,7 +27,7 @@ public class WarehouseServiceImpl implements WarehouseService {
 
     private String url = "http://localhost:9001";
 
-    final String fooResourceUrl = "http://localhost:9001/warehouse/items";
+    final String fooResourceUrl = "http://warehouse:9001/warehouse/items";
 
     @Override
     public List<WarehouseAdresses> getAllwarehouseAddress() {
